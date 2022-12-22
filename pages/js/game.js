9d7c854c8957a6093c1655245123cdebe51a6108
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2022-12-21 23:10:16 -0300

    bug resolvido
    resolvi o bug da carta extra

--- pages/js/game.js
+++ pages/js/game.js
@@ -1,5 +1,21 @@
 const grid = document.querySelector('.grid');
 
+const characters = [
+
+   'beth',
+   'jerry',
+   'jessica',
+   'meeseeks',
+   'morty',
+   'pessoa-passaro',
+   'pickle-rick',
+   'scroopy',
+   'summer',
+   'rick',   
+
+
+]
+
 const createElement = (tag, className) => {
     const element = document.createElement(tag);
     element.className = className;
@@ -7,18 +23,42 @@ const createElement = (tag, className) => {
 
 }
 
-const createCard = () => {
+const revealCard = ({ target }) => {
+   target.parentNode.classList.add('reveal-card');
+
+}
+
+const createCard = (character) => {
 
     const card = createElement('div', 'card');
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
 
+    front.style.backgroundImage = `url('./imagens/${character}.png')`;
+
     card.appendChild(front);
     card.appendChild(back);
 
+    card.addEventListener('click', revealCard);
+
     return card;     
 }
-createCard();
+const loadgame = () => {
+
+    const duplicateCharacters =[ ...characters, ...characters ];
+
+    const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
+
+    
+    duplicateCharacters.forEach((character) => {
+
+        const card = createCard(character);
+        
+        grid.appendChild(card);
+    })
+}
+
+loadgame();
 
 
 
