0f875d3f510439c63d86cc429187318448082183
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2022-12-23 20:11:09 -0300

    mudei de novo
    recolocação das pastas

--- game.js
+++ game.js
@@ -94,7 +94,7 @@ const createCard = (character) => {
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
 
-    front.style.backgroundImage = `url('../imagens/${character}.png')`;
+    front.style.backgroundImage = `url('./${character}.png')`;
 
     card.appendChild(front);
     card.appendChild(back);
